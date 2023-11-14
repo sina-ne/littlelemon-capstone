6b4d8cb3b3d0581c5d79c1c07b9f82fcc6894aa4
sina-ne <sina@neueder.net>
2023-11-14 14:10:54 +0100

    add date picker api

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="header">
-      <a>
+      <a href="#">
         <img src="../imgs/littlelemonlogo.png" alt="Logo of Little Lemon" />
       </a>
       <Nav />
